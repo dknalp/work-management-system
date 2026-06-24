f09cfc820a16588539723da4d71dce253a2c3d09
parsherr <dogukanalpt@gmail.com>
2026-06-24 18:00:49 +0300

    chore: remove unused useState import from site-header

--- frontend/components/site-header.tsx
+++ frontend/components/site-header.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useState } from "react"
 import { usePathname } from "next/navigation"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
